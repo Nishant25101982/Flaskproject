daedd3bbb3a0cea00981d2c5fc6726e96980c39e
Tyagi <ntyagi@dxc.com>
2025-11-23 07:53:55 +0530

    First Flask project for Training

---
+++ test.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/health")
+def liveness_probe():
+    return {'status': 'alive'}, 200
+
+@app.route("/test")
+def Final():
+    return render_template("hello.html")
+
+
+app.run(host="0.0.0.0",port=8000)
+
+
+
